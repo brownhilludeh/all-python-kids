5abf0f92e5cae9a78bb5b8fbbbf603627d7cbdab
Brownhill Udeh <85250041+brownhilludeh@users.noreply.github.com>
2022-01-30 12:14:53 -0800

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,4 +1,11 @@
-import datetime
-from playsound import playsound
-# Note to install playsound use pip install playsound in the cmd or terminal ..... enjoy 
 
+alarmHour = int(input("Enter Hour: "))
+alarmMin = int(input("Enter Minutes: "))
+alarmAm = input("am / pm: ")
+
+if alarmAm == "pm":
+    alarmHour +=12
+
+
+        
+#         ferel free to edit ths and update the code. 
