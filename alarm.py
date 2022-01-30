4918deb14f3607b2b58591d75665d2a323c4c893
Brownhill Udeh <85250041+brownhilludeh@users.noreply.github.com>
2022-01-30 12:14:00 -0800

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,19 +1,1 @@
 import datetime
-from playsound import playsound
-# Note to install playsound use pip install playsound in the cmd or terminal ..... enjoy 
-
-alarmHour = int(input("Enter Hour: "))
-alarmMin = int(input("Enter Minutes: "))
-alarmAm = input("am / pm: ")
-
-if alarmAm == "pm":
-    alarmHour +=12
-
-while True:
-    if alarmHour==datetime.datetime.now().hour and alarmMin==datetime.datetime.now().minute:
-        print('playing...')
-        playsound('punky.mp3')
-        break
-        
-        
-#         ferel free to edit ths and update the code. 
