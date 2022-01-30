08bfab19ef198bd4ae7c58876e3e38c1dbc4959a
Brownhill Udeh <85250041+brownhilludeh@users.noreply.github.com>
2022-01-30 12:15:13 -0800

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -6,6 +6,11 @@ alarmAm = input("am / pm: ")
 if alarmAm == "pm":
     alarmHour +=12
 
-
+while True:
+    if alarmHour==datetime.datetime.now().hour and alarmMin==datetime.datetime.now().minute:
+        print('playing...')
+        playsound('punky.mp3')
+        break
+        
         
 #         ferel free to edit ths and update the code. 
